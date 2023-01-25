aea966b46925b9aa6ce818a2d46f98dd881adfe9
Marcin Skic <marcinskicoficjal@gmail.com>
2023-01-25 23:15:18 +0100

    Create app logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Habit Keeper - Uni Project</title>
+    <title>Habit Tracker - Uni Project</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ URL::asset('css/normalize.css'); }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/main.css'); }}">
+    <link rel="icon" type="image/x-icon" href="{{ URL::asset('favicon.ico'); }}">
 </head>
 <body>
-    <header>
-        <h1 id="logo">Habit Keeper</h1>
+    <header class="top-bar">
+        <h1 id="logo">Habit Forge</h1>
         <nav>
             <a href="{{ url('/features') }}">Features</a>
             <a href="{{ url('/about') }}">About</a>
